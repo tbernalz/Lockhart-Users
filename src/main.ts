f3e84c0a13a5b8d2a15b41ad628e680dc5e1672c
Tomás Bernal <tbernalz@eafit.edu.co>
2025-05-07 21:30:49 -0500

    chore(app) enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
